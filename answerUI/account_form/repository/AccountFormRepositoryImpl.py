188435885768eb46c92e2cdc524f863575cdb97c
bikjo99 <bikjo99@gmail.com>
2024-01-03 15:45:24 +0900

    add:[이성용] 서버에게 회원탈퇴 요청 시 재질문(y/n) 추가 [PTAN-T2-UI-5]

--- answerUI/account_form/repository/AccountFormRepositoryImpl.py
+++ answerUI/account_form/repository/AccountFormRepositoryImpl.py
@@ -36,14 +36,16 @@ class AccountFormRepositoryImpl(AccountFormRepository):
         print('\033[31m'+"회원 탈퇴를 진행합니다."+'\033[0m')
         userInputId = KeyboardInput.getKeyboardInput("아이디를 입력하세요:")
         userInputPassword = KeyboardInput.getKeyboardInput("비밀번호를 입력하세요:")
-        return userInputId, userInputPassword
-        # userInputTrans = KeyboardInput.getKeyboardInput('\033[31m'+"회원 탈퇴를 진행할까요? (y/n):"+'\033[0m')
-        # print(userInputId, userInputPassword, userInputTrans)
-        # if userInputTrans == "b'y\n" or userInputTrans == "b'Y\n":
-        #     return userInputId, userInputPassword
-        # else:
-        #     # 회원 탈퇴를 취소할 경우 되돌아가는 경로가 필요함
-        #     return None, None
+        userInputTrans = KeyboardInput.getKeyboardInput('\033[31m'+"회원 탈퇴를 진행할까요? (y/n):"+'\033[0m')
+        if userInputTrans is not None:
+            # b'입력값\n 을 입력값 으로 변환 (예를들어 b'y\n 을 y 으로 변환)
+            newUserInputTrans = userInputTrans.decode().strip()
+            print(userInputId, userInputPassword, newUserInputTrans)
+        if newUserInputTrans == "y" or newUserInputTrans == "Y":
+            return userInputId, userInputPassword
+        else:
+            # 회원 탈퇴를 취소할 경우 되돌아가는 경로가 필요함
+            return
 
     def createProgramCloseForm(self):
         return
