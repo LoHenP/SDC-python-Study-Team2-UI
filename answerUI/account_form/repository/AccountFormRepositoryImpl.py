f0763d7cd6ff06bdbdc4cbc151c359ffa9e9a8cb
bikjo99 <bikjo99@gmail.com>
2024-01-04 11:49:36 +0900

    feat:[이성용] 글자색 변경 [PTAN-T2-UI-x]

--- answerUI/account_form/repository/AccountFormRepositoryImpl.py
+++ answerUI/account_form/repository/AccountFormRepositoryImpl.py
@@ -22,34 +22,35 @@ class AccountFormRepositoryImpl(AccountFormRepository):
         return cls.__instance
 
     def createAccountRegisterForm(self):
-        userInputId = KeyboardInput.getKeyboardInput("아이디를 입력하세요:")
-        userInputPassword = KeyboardInput.getKeyboardInput("비밀번호를 입력하세요:")
+        userInputId = KeyboardInput.getKeyboardInput('\033[95m'+"아이디를 입력하세요: "+'\033[0m')
+        userInputPassword = KeyboardInput.getKeyboardInput('\033[95m'+"비밀번호를 입력하세요: "+'\033[0m')
         return userInputId, userInputPassword
 
 
     def AccountLoginForm(self):
-        userInputId = KeyboardInput.getKeyboardInput("아이디를 입력하세요:")
-        userInputPassword = KeyboardInput.getKeyboardInput("비밀번호를 입력하세요:")
+        userInputId = KeyboardInput.getKeyboardInput('\033[95m'+"아이디를 입력하세요: "+'\033[0m')
+        userInputPassword = KeyboardInput.getKeyboardInput('\033[95m'+"비밀번호를 입력하세요: "+'\033[0m')
         return userInputId, userInputPassword
 
     def AccountLogoutForm(self):
-        print("로그아웃합니다.")
+        print('\033[31m \033[107m'+" 로그아웃합니다. "+'\033[0m')
         return
 
     def AccountDeleteForm(self):
-        print('\033[31m'+"회원 탈퇴를 진행합니다."+'\033[0m')
-        userInputId = KeyboardInput.getKeyboardInput("아이디를 입력하세요:")
-        userInputPassword = KeyboardInput.getKeyboardInput("비밀번호를 입력하세요:")
-        userInputTrans = KeyboardInput.getKeyboardInput('\033[31m'+"회원 탈퇴를 진행할까요? (y/n):"+'\033[0m')
-        if userInputTrans is not None:
-            # b'입력값\n 을 입력값 으로 변환 (예를들어 b'y\n 을 y 으로 변환)
-            newUserInputTrans = userInputTrans.decode().strip()
-            print(userInputId, userInputPassword, newUserInputTrans)
-        if newUserInputTrans == "y" or newUserInputTrans == "Y":
-            return userInputId, userInputPassword
-        else:
-            # 회원 탈퇴를 취소할 경우 되돌아가는 경로가 필요함
-            return
+        print('\033[31m \033[107m'+" 회원 탈퇴를 진행합니다. "+'\033[0m')
+        return
+        # userInputId = KeyboardInput.getKeyboardInput("아이디를 입력하세요:")
+        # userInputPassword = KeyboardInput.getKeyboardInput("비밀번호를 입력하세요:")
+        # userInputTrans = KeyboardInput.getKeyboardInput('\033[31m'+"회원 탈퇴를 진행할까요? (y/n):"+'\033[0m')
+        # if userInputTrans is not None:
+        #     # b'입력값\n 을 입력값 으로 변환 (예를들어 b'y\n 을 y 으로 변환)
+        #     newUserInputTrans = userInputTrans.decode().strip()
+        #     print(userInputId, userInputPassword, newUserInputTrans)
+        # if newUserInputTrans == "y" or newUserInputTrans == "Y":
+        #     return userInputId, userInputPassword
+        # else:
+        #     # 회원 탈퇴를 취소할 경우 되돌아가는 경로가 필요함
+        #     return
 
     def createProgramCloseForm(self):
         return
